abe7d8b8f97de0e50260e367796527d9cfb8dc0a
Roi Avinoam <avinoamr@gmail.com>
2016-10-21 11:48:35 +0300

    Setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="panoply_dropbox",
+    version="1.0.7",
+    description="Panoply Data Source for the Dropbox API",
+    author="Roi Avinoam",
+    author_email="roi@panoply.io",
+    url="http://panoply.io",
+    install_requires=[
+        "panoply-python-sdk",
+        "dropbox"
+    ],
+
+    # place this package within the panoply package namespace
+    package_dir={"panoply": ""},
+    packages=["panoply.dropbox"]
+)
