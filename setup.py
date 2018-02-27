ddeba9a03cea4a461a0c0fd9cc473fd5c6bb657b
oshribin <oshri@panoply.io>
2018-02-27 11:00:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="panoply_dropbox",
-    version="1.0.7",
+    version="1.0.8",
     description="Panoply Data Source for the Dropbox API",
     author="Roi Avinoam",
     author_email="roi@panoply.io",
